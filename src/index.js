27d890bdab2390453f9d4a040f80b10e95a80b9d
Sylvain DNS <sylvain.denyse@gmail.com>
2021-01-22 16:37:56 +0100

    feat: setup hmr

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function render(Component) {
+  ReactDOM.render(<Component />, document.getElementById('root'))
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App')
+    render(NextApp)
+  })
+}
